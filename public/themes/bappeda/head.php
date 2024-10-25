f39f65064a5c5621f780db479d687c9e9215dfdf
syukron <syukron488@gmail.com>
2024-10-25 19:05:36 +0700

    open function to block copy, cut, pasti etc.

--- public/themes/bappeda/head.php
+++ public/themes/bappeda/head.php
@@ -182,7 +182,7 @@
             border-bottom: 2px solid #dc8b14;
         }
     </style>
-    <script>
+    <!-- <script>
         document.addEventListener('contextmenu', function(event) {
             event.preventDefault();
         });
@@ -201,7 +201,7 @@
         document.addEventListener('paste', function(event) {
             event.preventDefault();
         });
-    </script>
+    </script> -->
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5321156983100513"
      crossorigin="anonymous"></script>
     <script async src="https://fundingchoicesmessages.google.com/i/pub-5321156983100513?ers=1" nonce="F9qY9xKWTyZynsohbAnE7g"></script><script nonce="F9qY9xKWTyZynsohbAnE7g">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>
